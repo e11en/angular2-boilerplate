677b7833a6561ea155150eff3ea3cff8359ecd4c
elangelaar <ellenlangelaar@gmail.com>
2017-06-08 10:29:05 +0200

    Minor button change to dialog

--- src/app/core/material/dialog.component.ts
+++ src/app/core/material/dialog.component.ts
@@ -6,8 +6,8 @@ import { MdDialogRef } from '@angular/material';
     template: `<h1 md-dialog-title>Dialog</h1>
     <div md-dialog-content>What would you like to do?</div>
     <div md-dialog-actions>
-        <button md-button md-dialog-close="Option 1">Option 1</button>
-        <button md-button md-dialog-close="Option 2">Option 2</button>
+        <button md-raised-button color="primary" md-dialog-close="Option 1">Option 1</button>
+        <button md-raised-button color="accent" md-dialog-close="Option 2">Option 2</button>
     </div>`,
 })
 export class DialogComponent {
