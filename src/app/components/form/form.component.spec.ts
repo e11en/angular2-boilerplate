4283f414fd9c3de21a2a45d4f217d0de1056c318
elangelaar <ellenlangelaar@gmail.com>
2017-08-25 13:41:22 +0200

    Describe name changed

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -3,7 +3,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { CoreModule } from '../../core/core.module';
 import { FormComponent } from './form.component';
 
-describe('ExampleComponent', () => {
+describe('FormComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
